e0ec3db4316075fdcc1b3d05728d76bd8f171295
J. Germishuizen <johannes.germishuizen@siemens.com>
2017-03-20 14:26:21 +0100

    refs #25 only compact form

--- code/CDesign.m
+++ code/CDesign.m
@@ -1,7 +1,5 @@
-function Wnd = CDesign(varargin)
+function Wnd = CDesign(strval)
 
-  % CDesign('Qs',144,'p',20,'x',1,'nl',2,'yd',4,'m',3)
-  % CDesign('Qs',54,'p',6,'x',1,'nl',2,'yd',4,'m',3)
   % [Xsi,C,CRe,theta_m,Coil_Data,Wnd] = CDesign(varargin)
   % 
   % Calculate the winding properties and coil layout
@@ -25,18 +23,9 @@ function Wnd = CDesign(varargin)
   %
   % J.J. Germishuizen 2007-2017
 
-  if (nargin == 1)
-    paramPairs = varargin{1};
-    disp(paramPairs)
-    [Qs,p,nl,yd,m,x] = compact_arg(paramPairs);
-  elseif (nargin == 12)
-    paramPairs = varargin(1:end);
-    [Qs,p,nl,yd,m,x] = input_parameter(paramPairs);
-  else
-    error('Too few input arguments');
-  end
+  [Qs,p,nl,yd,m,x] = getargs(strval);
 
-  [Wnd] = properties(Qs,p,m,yd,nl,x);
+  Wnd = properties(Qs,p,nl,yd,m,x);
 
   [C,CRe,theta_m] = coilassign(Wnd.Qs,Wnd.p, ...
       Wnd.m,Wnd.yd,Wnd.nl,Wnd.x);
@@ -67,44 +56,6 @@ function Wnd = CDesign(varargin)
   Wnd.theta_ma = theta_ma;
 
 return;
-
-function [Qs,p,nl,yd,m,x,plot] = input_parameter(paramPairs)
-  %
-  % Process param-value pairs (no specific order)
-  % 
-  % Qs : number of stator slots
-  % p  : number of pole pairs
-  % m  : number of phases
-  % yd : coil pitch
-  % nl : number of layers
-  % x  : slot pitch factor
-  %
-  args = {};
-  for k = 1:2:length(paramPairs)
-    param = lower(paramPairs{k});
-    if (~ischar(param))
-      error('Optional parameter names must be strings');
-    end
-    value = paramPairs{k+1};
-    
-    switch (param)
-     case 'qs'
-      Qs = value;
-     case 'p'
-      p = value;
-     case 'm'
-      m = value;
-     case 'yd'
-      yd= value;
-     case 'nl'
-      nl = value;
-     case 'x'
-      x = value;
-     otherwise
-      error(['Unrecognized option ' param '.']);
-    end
-  end
-return
 
 function [C,CRe,theta_m] = coilassign(Qs,p,varargin)
   % 
@@ -246,84 +197,5 @@ function [C,CRe,theta_m] = coilassign(Qs,p,varargin)
               sumCRe = sum(abs(CRe)); 
           end
       end
-  end
-return
-
-function [Winding] = properties(Qs,p,m,yd,nl,x)
-  %
-  % Distinguish between following common divisors:
-  %
-  % 1. ts = gcd(Qs,p)
-  % 2. tc = gcd(Qc,p)
-  % 3. tq = gcd(Qs,2pm)
-
-  % Determine the coil sides per phase
-  % Wnd.cspp = Wnd.Qs/Wnd.m*Wnd.nl;
-
-  Winding = [];
-  Winding.Qs = Qs;
-  Winding.p = p;
-  Winding.m = m;
-  Winding.yd = yd;
-  Winding.nl = nl;
-  Winding.x = x;
-  if nl == 2
-      Qc = Qs;
-  else
-      Qc = Qs/2;
-  end
-
-  % Get the common divisors
-  ts  = gcd(Qs,p);
-  tc  = gcd(Qc,p);
-  tqs = gcd(Qs,2*p*m);
-  tqc = gcd(Qc,2*p*m);
-
-  % Get the number of coils per pole and phase
-  qcn = Qc/tqc;
-  qcd = (2*p*m)/tqc;
-
-  % Get the number of slots per pole and phase
-  qsn = Qs/tqs;
-  qsd = (2*p*m)/tqs;
-
-  % Average coil pitch
-  yp = Qs/(2*p);
-
-  % Calculate yd if the user specified yd = 0. Coil span should be at least
-  % one.
-  if yd == 0    
-      yd = floor(yp);
-      if yd <= 0; yd = 1; end;
-  end
-
-  Winding.t   = tc;
-  Winding.qsn = qsn;
-  Winding.qsd = qsd;
-  Winding.qcn = qcn;
-  Winding.qcd = qcd;
-  Winding.yp = yp;
-  Winding.yd = yd;
-  Winding.nl = nl;
-  Winding.Qbasic = Qs/tc;
-  Winding.pbasic = p/tc;
-  Winding.Qc = Qc;
-
-  if mod(qcd,2) == 0
-      r = m;
-  else
-      r = 2*m;
-  end
-
-  if ts<p
-      Winding.subharmonic = true;
-  else
-      Winding.subharmonic = false;
-  end
-
-  if mod(Winding.Qs/Winding.t,Winding.m)==0
-      Winding.feasable = true;
-  else
-      Winding.feasable = false;
   end
 return
